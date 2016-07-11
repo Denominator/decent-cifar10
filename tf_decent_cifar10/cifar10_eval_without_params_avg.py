eedd29743f357ee25c41755048f6a9cf2d8ebebc
Marcin Moczulski <ms13mm@woma.cs.ox.ac.uk>
2016-07-11 22:56:16 +0100

    experiment prefix used in eval without paramsavg  as well

--- tf_decent_cifar10/cifar10_eval_without_params_avg.py
+++ tf_decent_cifar10/cifar10_eval_without_params_avg.py
@@ -48,11 +48,11 @@ import cifar10
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_string('eval_dir', './tmp/cifar10_eval',
+tf.app.flags.DEFINE_string('eval_dir', './' + FLAGS.experiment_prefix + '/cifar10_eval',
                            """Directory where to write event logs.""")
 tf.app.flags.DEFINE_string('eval_data', 'test',
                            """Either 'test' or 'train_eval'.""")
-tf.app.flags.DEFINE_string('checkpoint_dir', './tmp/cifar10_train',
+tf.app.flags.DEFINE_string('checkpoint_dir', './' + FLAGS.experiment_prefix + '/cifar10_train',
                            """Directory where to read model checkpoints.""")
 tf.app.flags.DEFINE_integer('eval_interval_secs', 60 * 5,
                             """How often to run the eval.""")
